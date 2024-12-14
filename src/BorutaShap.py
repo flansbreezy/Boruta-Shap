Fix: Taking mean over the wrong dimension
The tree explainer outputs log odds for for each class. By taking a mean of the 0th dim, we are reducing this object to just 2 values - one of for each class in a binary classification problem.

Instead, we need to take the mean of the 2nd dimension, which after the previous mean is the 2 classes.

src/BorutaShap.py
@@ -822,7 +822,6 @@ class BorutaShap:
                                        feature_perturbation = "tree_path_dependent",
                                        approximate = True)
 
-
         if self.sample:
 
 
@@ -840,7 +839,7 @@ class BorutaShap:
 
                 elif len(self.shap_values.shape) == 3:
                     self.shap_values = np.abs(self.shap_values).sum(axis=0)
-                    self.shap_values = self.shap_values.mean(0)
+                    self.shap_values = self.shap_values.mean(1)
 
                 else:
                     self.shap_values = np.abs(self.shap_values).mean(0)
@@ -864,7 +863,7 @@ class BorutaShap:
 
                 elif len(self.shap_values.shape) == 3:
                     self.shap_values = np.abs(self.shap_values).sum(axis=0)
-                    self.shap_values = self.shap_values.mean(0)
+                    self.shap_values = self.shap_values.mean(1)
 
                 else:
                     self.shap_values = np.abs(self.shap_values).mean(0)